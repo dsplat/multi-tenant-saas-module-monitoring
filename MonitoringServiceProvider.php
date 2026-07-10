8657d05c0df120ef9ae6d3217580d892d5c6f5b4
luoyueliang <luoyueliang@gmail.com>
2026-07-11 03:51:10 +0800

    sync: ci: 添加代码质量检查 (Pint + PHPStan)

--- MonitoringServiceProvider.php
+++ MonitoringServiceProvider.php
@@ -3,10 +3,10 @@
 namespace MultiTenantSaas\Modules\Monitoring;
 
 use MultiTenantSaas\Modules\Contracts\ModuleServiceProvider;
-use MultiTenantSaas\Services\MetricsService;
-use MultiTenantSaas\Services\SlaService;
 use MultiTenantSaas\Services\AlertService;
+use MultiTenantSaas\Services\MetricsService;
 use MultiTenantSaas\Services\PerformanceService;
+use MultiTenantSaas\Services\SlaService;
 
 class MonitoringServiceProvider extends ModuleServiceProvider
 {
